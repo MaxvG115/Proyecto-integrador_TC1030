6535c4a89dc420cc36050d8ee9abdbec8a54f91e
Manuelo247 <83720461+Manuelo247@users.noreply.github.com>
2021-06-12 15:55:58 -0700

    Creacion de Videos.h
    Manuel Eduardo Ochoa Obezo A00227718

---
+++ Videos.h
@@ -0,0 +1,39 @@
+#ifndef VIDEOS_H
+#define VIDEOS_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class Videos
+{
+  protected:
+    int ID;
+    string nombre;
+    int duracion; //en minutos
+    string genero;
+    float calificacion;
+
+  public:
+    Videos();
+    Videos(int, string, int, string);
+
+    int get_ID();
+    string get_Nombre();
+    int get_Duracion();
+    string get_Genero();
+    float get_Calificacion();
+
+    void set_ID(int);
+    void set_Nombre(string);
+    void set_Duracion(int);
+    void set_Genero(string);
+    void set_Calificacion(float);
+
+    virtual void evaluacion(float);
+
+};
+
+#endif
